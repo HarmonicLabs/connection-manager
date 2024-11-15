6a3b9002608f09117ae721d4a7fa23e1a4e007d2
RaffaBux <raffaelebussolotto@gmail.com>
2024-11-15 16:47:47 +0100

    added cm initialization

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
 import { ConnectionManager } from "./ConnectionManager/ConnectionManager";
-import { OutboundGovernor } from "./Governors/OutboundGovernor";
-import { InboundGovernor } from "./Governors/InboundGovernor";
-import { Logger, LogLevel } from "../utils/Logger";
+import { getClientIp as getClientIpFromReq } from "request-ip";
+import { Logger, LogLevel } from "./utils/Logger";
+import { WebSocketServer } from "ws";
+import express from "express";
+import http from "http";
+
+// initializations
 
 const logger = new Logger({ logLevel: LogLevel.DEBUG });
+const connectionManager = new ConnectionManager( logger );
 
-const connectionManager = new ConnectionManager();
+// wss initializations
 
-const outboundGovernor = new OutboundGovernor( connectionManager );
+const app = express();
+app.use( express.json() );
+app.set( "trust proxy", 1 );
 
-const inboundGovernor = new InboundGovernor( connectionManager );
+const http_server = http.createServer( app );
+const wsServer = new WebSocketServer({ server: http_server, path: "/connection-manager", maxPayload: 512 });
+
+http_server.listen((3001), () => {    
+    logger.debug("!- THE SERVER IS LISTENING ON PORT 3001 -!\n")
+});
+
+// wss events
+
+process.on( "beforeExit", () => connectionManager.terminateAllConnections() );
+wsServer.on( "close", () => connectionManager.terminateAllConnections() );
+
+wsServer.on( "error", console.error );
+
+wsServer.on( "connection", async ( client, req ) => {
+    const ip = getClientIpFromReq( req );
+
+    if( !ip )
+    {
+        logger.error("!- NO IP ADDRESS FOUND -!\n");
+        return client.terminate();
+    }
+
+    connectionManager.setConnection( client, ip );
+
+    client.on( "error", console.error );
+    // client.on("message", handleClientMessage.bind( client ));
+} );
