fdad77445d0b40d22061d06be13a5aafe9a0196d
RaffaBux <raffaelebussolotto@gmail.com>
2024-11-15 16:47:32 +0100

    added connection manager base functionalities

--- src/ConnectionManager/ConnectionManager.ts
+++ src/ConnectionManager/ConnectionManager.ts
@@ -1,28 +1,73 @@
+import { getWsClientIp, setWsClientIp } from "../utils/clientProps";
+import { OutboundGovernor } from "../Governors/OutboundGovernor";
+import { InboundGovernor } from "../Governors/InboundGovernor";
 import { Connection, ConnectionState } from "../types/Connection";
+import { Logger } from "../utils/Logger";
+import { WebSocket } from "ws";
+
 
 export class ConnectionManager 
 {
-    private connections: Map<string, Connection> = new Map();
-  
-    constructor() {}
+    private connections: Map<string, Connection>;
+    private outboundGovernor: OutboundGovernor;
+    private inboundGovernor: InboundGovernor;
+
+    logger: Logger;
+
+    constructor( logger: Logger ) 
+    {
+        this.connections = new Map();
+        this.outboundGovernor = new OutboundGovernor( this );
+        this.inboundGovernor = new InboundGovernor( this );
+        this.logger = logger;
+    }
 
     async getConnections(): Promise< Map<string, Connection> > 
     {
         return this.connections;
     }
 
-    async setConnection( connection: Connection ): Promise<void> 
+    async setConnection( client: WebSocket, ip: string ): Promise<void> 
     {
-        this.connections.set( connection.peerAddress, connection );
+        const connection: Connection = {
+            state: ConnectionState.Cold,
+            type: "outbound",
+            webSocket: client
+        };
+        
+        this.connections.set( ip, connection );
     }
 
     async updateConnection( connection: Connection ): Promise<void> 
     {
-        this.connections.set( connection.peerAddress, connection );
+        const ip = getWsClientIp( connection.webSocket );
+        this.connections.set( ip, connection );
     }
 
     async removeConnection( connection: Connection ): Promise<void> 
     {
-        this.connections.delete( connection.peerAddress );
+        const ip = getWsClientIp( connection.webSocket );
+        this.connections.delete( ip );
+    }
+
+    async terminateConnection( address: string ) 
+    {
+        this.logger.debug("!- TERMINATING THE CONNECTION -!\n");
+
+        const connection = this.connections.get( address );
+
+        if( !connection ) return;
+        this.removeConnection( connection );
+    }
+
+    async terminateAllConnections()
+    {
+        this.logger.debug("!- TERMINATING ALL THE CONNECTIONS -!\n");
+
+        for( const address of this.connections.keys() )
+        {
+            // client.send( closeMsg );
+            this.terminateConnection( address );
+        }
     }
 }
