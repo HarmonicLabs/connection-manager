d699762b208ed6b10bd7cca0c4d9f062c15a0d7c
RaffaBux <raffaelebussolotto@gmail.com>
2024-11-18 12:44:23 +0100

    cm shrinked to base outbound governor functionalities

--- src/ConnectionManager/ConnectionManager.ts
+++ src/ConnectionManager/ConnectionManager.ts
@@ -1,10 +1,7 @@
-import { getWsClientIp, setWsClientIp } from "../utils/clientProps";
 import { OutboundGovernor } from "../Governors/OutboundGovernor";
 import { InboundGovernor } from "../Governors/InboundGovernor";
-import { Connection, ConnectionState } from "../types/Connection";
+import { Connection } from "../types/Connection";
 import { Logger } from "../utils/Logger";
-import { WebSocket } from "ws";
-
 
 export class ConnectionManager 
 {
@@ -27,47 +24,8 @@ export class ConnectionManager
         return this.connections;
     }
 
-    async setConnection( client: WebSocket, ip: string ): Promise<void> 
+    async terminateAllConnections(): Promise<void>
     {
-        const connection: Connection = {
-            state: ConnectionState.Cold,
-            type: "outbound",
-            webSocket: client
-        };
-        
-        this.connections.set( ip, connection );
-    }
-
-    async updateConnection( connection: Connection ): Promise<void> 
-    {
-        const ip = getWsClientIp( connection.webSocket );
-        this.connections.set( ip, connection );
-    }
-
-    async removeConnection( connection: Connection ): Promise<void> 
-    {
-        const ip = getWsClientIp( connection.webSocket );
-        this.connections.delete( ip );
-    }
-
-    async terminateConnection( address: string ) 
-    {
-        this.logger.debug("!- TERMINATING THE CONNECTION -!\n");
-
-        const connection = this.connections.get( address );
-
-        if( !connection ) return;
-        this.removeConnection( connection );
-    }
-
-    async terminateAllConnections()
-    {
-        this.logger.debug("!- TERMINATING ALL THE CONNECTIONS -!\n");
-
-        for( const address of this.connections.keys() )
-        {
-            // client.send( closeMsg );
-            this.terminateConnection( address );
-        }
+        this.outboundGovernor.terminateAllConnections( this.connections );
     }
 }
